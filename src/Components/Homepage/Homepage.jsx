0db5e37a7e39fdcd542c22a2b2e3d949b0be006d
stvial <stanislas.vidal@gmail.com>
2021-11-25 22:43:12 +0100

    Create & connect Backend server

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -1,8 +1,15 @@
 import Login from "../Login/Login.jsx"
 import "./Homepage.css"
 
+
+
+
 const HomePage = () => {
+
+
+    
     return (
+        
  <Login />
     )
 }
--- src/Components/Welcomepage/Welcomepage.jsx
+++ src/Components/Welcomepage/Welcomepage.jsx
@@ -1,6 +1,27 @@
+import axios from 'axios';
+import React, { useEffect } from "react";
 import ButtonCards from "../Buttoncards/Buttoncards.jsx"
 
+
+
 const WelcomePage = () => {
+
+    
+    const getDataUser = () => {
+        
+        
+        axios.get(`http://localhost:5000/user`, {credentials: 'include'})
+        .then(response => {
+            console.log(response.data)
+        })
+    }
+
+
+
+    useEffect (()=>{
+        getDataUser()
+    },[])
+
     return (
         <div>
             <ButtonCards />
