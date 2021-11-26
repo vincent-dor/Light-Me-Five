735f92eb012b7119d67e28b1ddfd847e6886064b
stvial <stanislas.vidal@gmail.com>
2021-11-26 02:15:05 +0100

    Add PUT

--- src/Components/Welcomepage/Welcomepage.jsx
+++ src/Components/Welcomepage/Welcomepage.jsx
@@ -6,22 +6,46 @@ import ButtonCards from "../Buttoncards/Buttoncards.jsx"
 
 const WelcomePage = () => {
 
-    
+
+
     const getDataUser = () => {
-        
-        
-        axios.get(`http://localhost:5000/user`, {credentials: 'include'})
+            
+        axios.get(`http://localhost:5000/user`)
         .then(response => {
             console.log(response.data)
         })
     }
+    getDataUser()
+
+
+    //POST
+
+    // useEffect(() => {
+    //     axios.post(`http://localhost:5000/user`,  {
+    //         nickname:"Tom" ,
+    //         favorite_style_one:"JAZZ" ,
+    //         favorite_style_two:"FUNK" ,
+    //         artist_one:"xxxx" ,
+    //         artist_two:"xxxx" ,
+    //         artist_three:"xxxx"
+    //     })
+    // },[])
 
 
 
-    useEffect (()=>{
-        getDataUser()
+    //PUT
+    //add USESTATE id
+      
+    useEffect(()=> {
+        const id=1;
+        axios.put(`http://localhost:5000/user/${id}`, {
+            artist_one:"zz66",
+            artist_two: "zzz66",
+            artist_three: "zzzz66",
+        })
+
     },[])
-
+        
     return (
         <div>
             <ButtonCards />
